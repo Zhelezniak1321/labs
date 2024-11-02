3b47224a36b515d76589c26595a3b54502c6bc77
zhelezniak <oleksandrzhelez@gmail.com>
2024-11-03 00:06:03 +0200

    add lab1

---
+++ lab1/import math.py
@@ -0,0 +1,7 @@
+import math
+from math import cos
+z = 9.761
+result = (z**6 + 13*z**4 + 7*z**3 + 14*z**2 - 2*z + 21 ) * cos(z**6)
+ 
+print(round(result))
+
---
+++ lab1/lab1.py
@@ -0,0 +1,15 @@
+
+movie = "Inception"
+print(f"Мій улюблений фільм: {movie}")
+
+
+is_employee = False
+is_employer = True
+print(f"is_employee AND is_employer: {is_employee and is_employer}")
+print(f"is_employee OR is_employer: {is_employee or is_employer}")
+
+
+is_sunny = True
+is_raining = False
+print(f"NOT is_sunny: {not is_sunny}")
+print(f"NOT is_raining: {not is_raining}")
